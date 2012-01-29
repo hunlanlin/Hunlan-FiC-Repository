1139b0237194ffa2434b55d42ae71d241d36127f
Hunlan <hunlan@ee.washington.edu>
2012-01-28 17:40:23 -0800

    setting up heroku

---
+++ FiC_Django/FiC_Django/app.py
@@ -0,0 +1,16 @@
+'''
+Created on Jan 28, 2012
+
+@author: Hunlan
+'''
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Python!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
